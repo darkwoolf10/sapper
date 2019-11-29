85e818ca0774201096b2d5deb231a59e681b1a51
darkwoolf10 <woolftreuser@gmail.com>
2019-11-29 23:30:57 +0200

    make refresh app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import refresh from './refresh.svg';
 import Grid from './Grid';
 
 class App extends React.Component {
+  refresh = () => {
+    window.location.reload();
+  };
+
   render() {
     return  <div className="box">
-      <Grid />
+      <img src={refresh} className='reset' onClick={this.refresh} alt=""/>
+      <div className="container">
+        <Grid />
+      </div>
     </div>;
   };
 }
