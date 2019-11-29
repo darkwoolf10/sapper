55f4ca68e429a2979455a2add928ec5a532675fa
darkwoolf10 <woolftreuser@gmail.com>
2019-11-29 23:08:19 +0200

    make sapper game

--- src/Grid.js
+++ src/Grid.js
@@ -2,26 +2,40 @@ import React from 'react';
 import './App.css';
 
 class Grid extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+            goodItemsCount: 0,
+            grid: [
+                [0,1,0,0,0,1,0,0,0,1],
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,1,0,0,0,1,1,1,0,0],
+                [0,0,0,0,0,0,0,1,0,0],
+                [0,0,0,0,1,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,1],
+                [0,0,0,0,1,0,0,0,0,0],
+                [0,0,1,0,0,0,0,1,0,0],
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,1,0,0,1,0,0,0,0,0],
+            ]
+        };
+    }
+
+    componentDidMount() {
+        this.findGoodItemsCount();
+    }
+
     createGrid = () => {
         let grid = [];
-        const data = [
-            [0,1,0,0,0,1,0,0,0,1],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,1,0,0,0,1,1,1,0,0],
-            [0,0,0,0,0,0,0,1,0,0],
-            [0,0,0,0,1,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,1],
-            [0,0,0,0,1,0,0,0,0,0],
-            [0,0,1,0,0,0,0,1,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,1,0,0,1,0,0,0,0,0],
-        ];
 
-        for(let i = 0; i < data.length; i++) {
+        for(let i = 0; i < this.state.grid.length; i++) {
             let children = [];
 
-            for (let j = 0; j < data[i].length; j++) {
-                children.push(<td key={i + "," + j} data-status={data[i][j]} onClick={(element) => this.clickItem(element)}/>);
+            for (let j = 0; j < this.state.grid[i].length; j++) {
+                children.push(<td key={i + "," + j} data-status={this.state.grid[i][j]} onClick={
+                    (element) => this.clickItem(element)
+                }/>);
             }
 
             grid.push(<tr key={i}>{children}</tr>);
@@ -30,9 +44,41 @@ class Grid extends React.Component {
         return grid;
     };
 
+    findGoodItemsCount = () => {
+        for(let i = 0; i < this.state.grid.length; i++) {
+            for (let j = 0; j < this.state.grid[i].length; j++) {
+                if (this.state.grid[i][j] === 0) {
+                    // eslint-disable-next-line react/no-direct-mutation-state
+                    this.state.goodItemsCount++
+                }
+            }
+        }
+    };
+
+    boom = (item) => {
+        item.className += 'boom';
+
+        window.setTimeout(() => {
+            alert('The end!');
+            window.location.reload();
+        }, 50);
+    };
+
     clickItem = (element) => {
-        const status = element.target.dataset.status;
-        status === 1 ? console.log(status) : console.log(status);
+        const item = element.target;
+        const status = item.dataset.status;
+
+        if (status === '1') {
+            this.boom(item);
+            return;
+        }
+
+        item.className += 'done';
+
+        // eslint-disable-next-line react/no-direct-mutation-state
+        if (++this.state.count === this.state.goodItemsCount) {
+            alert("You are a winner. Start over")
+        }
     };
 
     render() {
