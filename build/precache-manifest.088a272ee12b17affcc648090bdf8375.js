d2a850b4fdb77430d5e3afd78f2d9b5540653b69
darkwoolf10 <woolftreuser@gmail.com>
2019-11-29 23:41:44 +0200

    added build folder

---
+++ build/precache-manifest.088a272ee12b17affcc648090bdf8375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0aa73108b761691e35bf6439e0147df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5af3c0896284399ef8b",
+    "url": "/static/css/main.75378f43.chunk.css"
+  },
+  {
+    "revision": "8dcf8ab9a841d98e3440",
+    "url": "/static/js/2.f8495f49.chunk.js"
+  },
+  {
+    "revision": "c5af3c0896284399ef8b",
+    "url": "/static/js/main.c5260785.chunk.js"
+  },
+  {
+    "revision": "75de86ff9ec11f705067",
+    "url": "/static/js/runtime-main.d7555cd6.js"
+  },
+  {
+    "revision": "68fe39fd6da2dafa9558dee1c9c0d5ff",
+    "url": "/static/media/refresh.68fe39fd.svg"
+  }
+]);
